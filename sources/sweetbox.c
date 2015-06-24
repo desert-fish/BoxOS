03977d42d6f88bad6bd10793b5bcc2dc3dcc5c06
desert-fish <dead_fish@163.com>
2015-06-24 10:00:41 +0800

    modify main.c

--- sources/sweetbox.c
+++ sources/sweetbox.c
@@ -12,7 +12,6 @@
 //一些硬件驱动的PID 号码，这些号码固定不变
 #define LED1_PID 2	//红灯
 #define LED2_PID 3	//绿灯
-#define MOTOR_PID 4	//马达驱动
 //-------------------------------------
 #define ERROR_SHOW(n)  MT_CallService(LED1_PID,n,0)
 #define NORMAL_SHOW(n) MT_CallService(LED2_PID,n,0)
@@ -84,8 +83,7 @@ int main(){
 	MT_ChangeSP(0);
 	MT_InitSystem();
 	MT_AddTask((u32)Led1_Driver,LED1_PID,0,0);
-	MT_AddTask((u32)Led2_Driver,LED2_PID,0,0);	
-	//MT_AddTask((u32)Motor_Driver,MOTOR_PID,0,0);	
+	MT_AddTask((u32)Led2_Driver,LED2_PID,0,0);		
 	MT_AddTask((u32)proc1,0,0,0);
 	MT_AddTask((u32)proc2,0,0,0);
 	MT_StartSysTick();
